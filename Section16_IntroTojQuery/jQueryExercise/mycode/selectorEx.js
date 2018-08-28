95a4d1744de9b59598f737d6b34aebb9e911017b
Gautam Gupta <ggupta78@gmail.com>
2018-08-28 22:19:03 +0530

    Advanced jQuery

---
+++ Section16_IntroTojQuery/jQueryExercise/mycode/selectorEx.js
@@ -0,0 +1,8 @@
+$('div').css('background', 'purple');
+
+$('div.highlight').css('width', '200px');
+
+$('#third').css('border', '1px solid orange');
+
+$('div:first-of-type').css('color', 'pink');
+//$('div').first().css('color', 'pink');
