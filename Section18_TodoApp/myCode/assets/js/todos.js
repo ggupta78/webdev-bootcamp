a9fe8e654febf754a3b115e0fa176857b4b41a93
Gautam Gupta <ggupta78@gmail.com>
2018-09-07 15:11:10 +0530

    Todo list project partially completed

--- Section18_TodoApp/myCode/assets/js/todos.js
+++ Section18_TodoApp/myCode/assets/js/todos.js
@@ -1,1 +1,21 @@
-alert("connected!");
+//Check off a todo item when clicked
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+})
+
+//Click on X to remove todo item
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+})
+
+//Add new todo item from text box
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+})
